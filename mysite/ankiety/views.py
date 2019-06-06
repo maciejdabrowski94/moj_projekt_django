3038e22483be076b4508e683489ad96e1e740d25
Maciej Dabrowski <dabrowski94@gmail.com>
2019-06-06 21:55:10 +0200

    dodanie szablonu ankiety + strona 404

--- mysite/ankiety/views.py
+++ mysite/ankiety/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. Jesets na stronie ANKIETY.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'ankiety/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("Ogladasz pytanie: %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Pytanie nie istnieje")
+    return render(request, 'ankiety/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "Przegladasz wyniki pytania: %s."
