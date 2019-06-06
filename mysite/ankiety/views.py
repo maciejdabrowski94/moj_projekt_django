11e7de6d2abaf690440645fa3ae0327f70880ec0
Maciej Dabrowski <dabrowski94@gmail.com>
2019-06-06 22:41:04 +0200

    dodanie formularza do glosowania + frontend do wynikow ankiety

--- mysite/ankiety/views.py
+++ mysite/ankiety/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -17,8 +18,20 @@ def detail(request, question_id):
     return render(request, 'ankiety/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "Przegladasz wyniki pytania: %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'ankiety/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("Glosujesz w pytaniu: %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #wyswietl ponownie formularz do glosowania
+        return render(request, 'ankiety/detail.html', {
+            'question': question,
+            'error_message': "Nie dokonales wyboru",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('ankiety:results', args=(question.id,)))
