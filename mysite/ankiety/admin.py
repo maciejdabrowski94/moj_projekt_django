29b6161f9fd93637bec92406d4e391284f258ad0
Maciej Dabrowski <dabrowski94@gmail.com>
2019-06-07 19:46:32 +0200

    dodanie stylów oraz statycznych plików + drobna zmiana w admin panelu

--- mysite/ankiety/admin.py
+++ mysite/ankiety/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Data i godzina publikacji', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Question, QuestionAdmin)
